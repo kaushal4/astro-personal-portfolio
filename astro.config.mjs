6d9a15e94fee33e240310bc9d6cdb543b48c837f
Kaushal Damania <damaniakaushal4@gmail.com>
2024-08-31 23:12:45 -0400

    remove zeabur

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,4 @@
 import { defineConfig } from 'astro/config';
-import zeabur from '@zeabur/astro-adapter/serverless';
 
 // https://astro.build/config
-export default defineConfig({
-  output: 'server',
-  adapter: zeabur(),
-});
+export default defineConfig({});
